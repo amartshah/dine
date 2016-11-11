8d5b1e867c0301f6ec1ef31447b3e313bbe970fa
Devon D'Apuzzo <djdapz@aol.com>
2016-11-11 11:57:24 -0600

    merge fix

--- www/app/account/account.controller.js
+++ www/app/account/account.controller.js
@@ -1,60 +1,57 @@
 /* recommended */
-var AccountController = function($scope, $window, UserService){
-  $scope.test = "accounts page coming soon";
-  $scope.user = UserService.getUser();
-  $scope.state = {
-      error: undefined,
-      working: true
-  };
-<<<<<<< HEAD
-=======
+var AccountController = function($scope, $window, UserService) {
+    $scope.test = "accounts page coming soon";
+    $scope.user = UserService.getUser();
+    $scope.state = {
+        error: undefined,
+        working: true
+    };
     $scope.pos = 'one';
 
     $scope.center = {
-	lat: 42.0451,
-	lng: -87.6877,
-	zoom: 2
+        lat: 42.0451,
+        lng: -87.6877,
+        zoom: 2
     }
 
     $scope.markers = {}
 
     $scope.loc = navigator.geolocation.getCurrentPosition(function(position) {
-	$scope.center = {
-	    lat: position.coords.latitude,
-	    lng: position.coords.longitude,
-	    zoom: 20
-	}
+        $scope.center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+            zoom: 20
+        }
 
-	$scope.markers = {
-	    currentLoc: {
-		lat: $scope.center.lat,
-		lng: $scope.center.lng,
-		focus: true
-	    }
-	}
+        $scope.markers = {
+            currentLoc: {
+                lat: $scope.center.lat,
+                lng: $scope.center.lng,
+                focus: true
+            }
+        }
     });
 
-    
-  if($scope.user == undefined){
-      UserService.getAccount(
-          //successCallback
-          function(value){
-              $scope.user = value.data[0];
-              $scope.state.working = false;
+
+    if ($scope.user == undefined) {
+        UserService.getAccount(
+            //successCallback
+            function(value) {
+                $scope.user = value.data[0];
+                $scope.state.working = false;
 
 
-          },
-          //failureCallback
-          function(error){
-              $scope.error = error;
-              $scope.state.working = false;
-          }
-      );
-  }else{
-      $scope.state.working = false;
-  }
+            },
+            //failureCallback
+            function(error) {
+                $scope.error = error;
+                $scope.state.working = false;
+            }
+        );
+    } else {
+        $scope.state.working = false;
+    }
 
->>>>>>> 6a2a29e3631931d29b95c9dab7c1ec4f447299d0
 };
 
 angular
