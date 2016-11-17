e55678c64e91288fdda4eb4936d1022a35accb42
Devon DaPuzzo <devondapuzzo@dhcp-10-105-138-176.wireless.northwestern.private>
2016-11-17 17:58:58 -0600

    variable name in scss

--- www/app/services/app.services.js
+++ www/app/services/app.services.js
@@ -5,7 +5,7 @@ var appServices = angular.module('dine.services');
 appServices.factory("ConfigService", function() {
     var backendUrl = "https://eecs-394-dine-backend.herokuapp.com";
     // var backendUrl = 'localhost:3000';
-    var env = "dev"
+    var env = "prod";
     var industries = [
         {name: "Banking"},
         {name: "Creative"},
@@ -25,7 +25,7 @@ appServices.factory("ConfigService", function() {
 
     return {
         url: backendUrl,
-        industries: industries, 
+        industries: industries,
         env: env
     }
 })
