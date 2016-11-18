bd155104e5c21d759d2303c3f721369b575a581e
System Administrator <root@dhcp-10-105-138-176.wireless.northwestern.private>
2016-11-17 18:42:48 -0600

    working on aesthetics

--- www/app/services/app.services.js
+++ www/app/services/app.services.js
@@ -6,22 +6,25 @@ appServices.factory("ConfigService", function() {
     var backendUrl = "https://eecs-394-dine-backend.herokuapp.com";
     // var backendUrl = 'localhost:3000';
 
-    var env = "prod";
+    var env = "dev";
     var industries = [
-        {name: "Banking"},
-        {name: "Creative"},
-        {name: "Journalism"},
-        {name: "Entertainment"},
-        {name: "Retail"},
+        {name: "Agriculture"},
+        {name: "Arts"},
+        {name: "Construction"},
+        {name: "Consumer Goods"},
+        {name: "Corporate"},
+        {name: "Education"},
+        {name: "Finance"},
+        {name: "Government"},
+        {name: "Legal"},
+        {name: "Manufacturing"},
+        {name: "Media"},
+        {name: "Medical"},
+        {name: "Non-Profit"},
+        {name: "Recreational"},
         {name: "Service"},
-        {name: "Health Care"},
-        {name: "Banking"},
-        {name: "Comsumer Goods"},
-        {name: "Consulting"},
-        {name: "Pharmecuticals"},
         {name: "Technology"},
-        {name: "Travel"},
-        {name: "Finance"}
+        {name: "Transportation"}
     ]
 
     return {
